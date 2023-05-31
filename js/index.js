78d0b761f02c3a28cc6115edf2e0bbaf99242ed5
Bettina Justus <bettina.jus@gmail.com>
2023-05-31 12:57:57 +0200

    prevent admin privileges for non admin accounts

--- js/index.js
+++ js/index.js
@@ -1,65 +1,67 @@
-import { isAdmin, isValidUserData } from './authService.js';
+import { isAdmin, isValidUserData } from "./authService.js";
 import {
   activateAdminControls,
   getFormValuesById,
   getValueById,
   resetValueById,
   revokeAdminControls,
-} from './domHelper.js';
+} from "./domHelper.js";
 import {
   getAddressAndAddLocation,
   getAddressAndUpdateLocation,
   getCoordsAndAddLocation,
   getCoordsAndUpdateLocation,
-} from './geoService.js';
-import { removeLocation } from './locations.js';
-import { refreshLocationsList } from './locations.js';
-import { deleteLocationMarker } from './mapService.js';
-import { navigateOnClick, navigateToScreenById } from './routingService.js';
+} from "./geoService.js";
+import { removeLocation } from "./locations.js";
+import { refreshLocationsList } from "./locations.js";
+import { deleteLocationMarker } from "./mapService.js";
+import { navigateOnClick, navigateToScreenById } from "./routingService.js";
 
 // Initialize SPA
 window.onload = () => {
-  navigateToScreenById('login-screen');
+  navigateToScreenById("login-screen");
   // Initial load of stored Locations
   refreshLocationsList();
   // Form Bindings
-  document.getElementById('login-form').onsubmit = clickLogin;
-  document.getElementById('add-loc-form').onsubmit = clickAddLocation;
-  document.getElementById('update-loc-form').onsubmit = clickModifyLocation;
+  document.getElementById("login-form").onsubmit = clickLogin;
+  document.getElementById("add-loc-form").onsubmit = clickAddLocation;
+  document.getElementById("update-loc-form").onsubmit = clickModifyLocation;
   // Button Bindings
-  navigateOnClick('add-location-btn', 'add-screen');
-  document.getElementById('logout').onclick = clickLogout;
-  document.querySelectorAll('.back-to-main').forEach((element) => {
-    element.onclick = (e) => navigateToScreenById('main-screen');
+  navigateOnClick("add-location-btn", "add-screen");
+  document.getElementById("logout").onclick = clickLogout;
+  document.querySelectorAll(".back-to-main").forEach((element) => {
+    element.onclick = (e) => navigateToScreenById("main-screen");
   });
-  document.getElementById('delete-btn').onclick = clickDeleteLocation;
+  document.getElementById("delete-btn").onclick = clickDeleteLocation;
 };
 
 const clickLogin = (event) => {
   event.preventDefault();
-  const username = getValueById('username');
-  const password = getValueById('password');
+  const username = getValueById("username");
+  const password = getValueById("password");
   if (isValidUserData(username, password)) {
-    navigateToScreenById('main-screen');
+    navigateToScreenById("main-screen");
     if (isAdmin(username)) {
       activateAdminControls();
+    } else {
+      revokeAdminControls();
     }
   } else {
-    alert('The provided credential combination does not exist');
+    alert("The provided credential combination does not exist");
   }
-  resetValueById('username');
-  resetValueById('password');
+  resetValueById("username");
+  resetValueById("password");
 };
 
 const clickLogout = (event) => {
   event.preventDefault();
-  navigateToScreenById('login-screen');
+  navigateToScreenById("login-screen");
   revokeAdminControls();
 };
 
 const clickAddLocation = (event) => {
   event.preventDefault();
-  const locationInput = getFormValuesById('add-loc-form');
+  const locationInput = getFormValuesById("add-loc-form");
   const hasAdress = locationInput.street && locationInput.zipCode;
   const hasCoords = locationInput.lat && locationInput.lon;
 
@@ -68,13 +70,13 @@ const clickAddLocation = (event) => {
   } else if (hasCoords) {
     getAddressAndAddLocation(locationInput);
   } else {
-    alert('Bitte entweder Straße und PLZ oder Längen-/Breitengrad eintragen.');
+    alert("Bitte entweder Straße und PLZ oder Längen-/Breitengrad eintragen.");
   }
 };
 
 const clickModifyLocation = (event) => {
   event.preventDefault();
-  const locationInput = getFormValuesById('update-loc-form');
+  const locationInput = getFormValuesById("update-loc-form");
   const hasAdress = locationInput.street && locationInput.zipCode;
   const hasCoords = locationInput.lat && locationInput.lon;
   if (hasAdress) {
@@ -82,14 +84,14 @@ const clickModifyLocation = (event) => {
   } else if (hasCoords) {
     getAddressAndUpdateLocation(locationInput);
   } else {
-    alert('Bitte entweder Straße und PLZ oder Längen-/Breitengrad eintragen.');
+    alert("Bitte entweder Straße und PLZ oder Längen-/Breitengrad eintragen.");
   }
 };
 
 const clickDeleteLocation = (event) => {
   event.preventDefault();
-  const id = document.getElementById('hidden-id-field').innerText;
+  const id = document.getElementById("hidden-id-field").innerText;
   removeLocation(id);
   deleteLocationMarker(id);
-  navigateToScreenById('main-screen');
+  navigateToScreenById("main-screen");
 };
